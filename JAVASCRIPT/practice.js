25090026a88b9f1c7de6ac3a5b949beedb3cae7e
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-07-14 09:31:37 +0600

    dextop design

---
+++ JAVASCRIPT/practice.js
@@ -0,0 +1,26 @@
+//Calculate the summation of 1 to 10
+var sum = 0
+for (i = 0; i <= 10; i++) {
+    if (i % 2 === 0)
+    
+    console.log(sum + '+' + i + '=' + (sum + i))   
+    sum += i
+   
+    // console.log(sum +i)
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i <= 100; i++) {
+//     if(i%5===1)
+//     console.log(i)
+// }
